ea376fd837f304909c58467a8a0108c85737692f
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-25 11:54:15 +0300

    Redirecting Users

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
+import React, { useEffect } from 'react'
+
 
 const NotFound = () => {
+
+  let router = useRouter()
+
+  useEffect( () => {
+    setTimeout( () => {
+      router.push( '/' )
+    }, 3000 )
+  } )
+
   return (
     <div className='not-found'>
       <h1>Ooooops...</h1>
