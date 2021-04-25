8693527cd93cad88bd60f204394cf051a30fa3aa
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-25 08:59:08 +0300

    Add other Components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
@@ -15,6 +18,7 @@ export default function Home() {
         Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.
         Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
       </p>
+      <Footer />
     </div>
   )
 }
