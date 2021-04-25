45554375022b14b6d33cbf4ca29c9b21b2650751
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-25 14:30:34 +0300

    getStaticPaths

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,4 +1,18 @@
-import React from 'react'
+export const getStaticPaths = async () => {
+
+  let res = await fetch( 'http://jsonplaceholder.typicode.com/users' )
+  let data = await res.json()
+
+  let paths = data.map( ninja => {
+    return {
+      paras: { id: ninja.id.toString() }
+    }
+  } )
+  return {
+    paths,
+    fallback: false
+  }
+}
 
 const Details = () => {
   return (
